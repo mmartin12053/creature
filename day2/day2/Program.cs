5f1aabd607c43e9e62e49cea62d4346f4f6029ac
Bipbip <70155252+mmartin12053@users.noreply.github.com>
2021-08-31 09:43:33 -0400

    Add files via upload

---
+++ day2/day2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("what is your name? ");
+            Console.ReadLine();
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine("let him in. let him in. let him in. let him in. let him in. ");
+                Console.WriteLine("let him in. let him in. let him in. let him in. let him in. ");
+                Console.WriteLine("let him in. let him in. let him in. let him in. let him in. ");
+                Console.WriteLine("let him in. let him in. let him in. let him in. let him in. ");
+                Console.WriteLine("let him in. let him in. let him in. let him in. let him in. ");
+            }
+            Console.WriteLine("░░░░▄███▓███████▓▓▓░░░░");
+            Console.WriteLine("░░░███░░░▒▒▒██████▓▓░░░");
+            Console.WriteLine("░░██░░░░░░▒▒▒██████▓▓░░");
+            Console.WriteLine("░██▄▄▄▄░░░▄▄▄▄█████▓▓░░");
+            Console.WriteLine("░██░(◐)░░░▒(◐)▒███████▓▓░");
+            Console.WriteLine("░██░░░░░░░▒▒▒▒▒█████▓▓░");
+            Console.WriteLine("░██░░░▀▄▄▀▒▒▒▒▒█████▓▓░");
+            Console.WriteLine("░█░███▄█▄█▄███░█▒████▓▓░");
+            Console.WriteLine("░█░███▀█▀█▀█░█▀▀▒█████▓░");
+            Console.WriteLine("░█░▀▄█▄█▄█▄▀▒▒▒▒█████▓░");
+            Console.WriteLine("░████░░░░░░▒▓▓███████▓░");
+            Console.WriteLine("░▓███▒▄▄▄▄▒▒▒▒████████░");
+            Console.WriteLine("░▓▓██▒▓███████████████░");
+
+
+
+        }
+    }
+}
